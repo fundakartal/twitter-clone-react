aa61e7ce515265acdc420eedb8f20da40315dcef
fundakartal <kartalfunda01@gmail.com>
2022-06-29 15:37:35 +0300

    Edit layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Twitter</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className='min-h-screen flex max-w-[1265px] mx-auto'>
+        <Component {...pageProps} />
+      </main>
+
+    </>
+  )
 }
 
 export default MyApp
