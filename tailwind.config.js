b07dcec2e42422c023a00cf372071ddc1ca9b67b
fundakartal <kartalfunda01@gmail.com>
2022-06-29 15:35:24 +0300

    Extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,33 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: {
+          base: '#d6d9db',
+        },
+        blue: {
+          base: '#1d9bf0',
+          dark: '#1b8edb',
+          light: '#8ecdf8',
+          extralight: '#d2ebfc',
+        },
+        // black: {
+        //   base: 'rgb(15,20,25)',
+        //   light: '#536471',
+        // },
+        gray: {
+          base: 'rgba(15,20,25,0.2)',
+          dark: 'rgb(83, 100, 113)',
+          // light: '#e7e7e8',
+          light: '#eff3f4',
+          extralight: '#f7f9f9',
+        },
+      },
+    },
   },
   plugins: [],
 }
