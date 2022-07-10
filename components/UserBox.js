5cd1f46494a395bdf46c656b933b313f91397747
fundakartal <kartalfunda01@gmail.com>
2022-07-10 15:39:55 +0300

    Add userimg component and edit userbox component

--- components/UserBox.js
+++ components/UserBox.js
@@ -1,13 +1,10 @@
 import { DotsHorizontal } from './icons/Icon'
+import UserImg from './UserImg'
 
 export default function UserBox() {
   return (
     <div className='hidden xxs:sm:flex items-center justify-center hoverAnimation p-3'>
-      <img
-        src='https://pbs.twimg.com/profile_images/1446165567187132420/8XVo47k1_400x400.jpg'
-        alt='Profile image'
-        className='w-10 h-10 rounded-full xl:mr-3 '
-      />
+      <UserImg className='w-10 h-10 rounded-full xl:mr-3' />
       <div className='hidden xl:inline leading-5 '>
         <h4 className='font-bold'>funda</h4>
         <p className='text-gray-dark'>@fundakartal</p>
---
+++ components/UserImg.js
@@ -0,0 +1,9 @@
+export default function UserImg({ className }) {
+  return (
+    <img
+      src='https://pbs.twimg.com/profile_images/1446165567187132420/8XVo47k1_400x400.jpg'
+      alt='Profile image'
+      className={className}
+    />
+  )
+}
