436a397cfe7cbec7685b7978c8e9527d354cac1f
fundakartal <kartalfunda01@gmail.com>
2022-07-10 17:43:14 +0300

    Edit class

--- components/UserBox.js
+++ components/UserBox.js
@@ -4,7 +4,7 @@ import UserImg from './UserImg'
 export default function UserBox() {
   return (
     <div className='hidden xxs:sm:flex items-center justify-center hoverAnimation p-3'>
-      <UserImg className='w-10 h-10 rounded-full xl:mr-3' />
+      <UserImg className='w-10 h-10 xl:mr-3' />
       <div className='hidden xl:inline leading-5 '>
         <h4 className='font-bold'>funda</h4>
         <p className='text-gray-light'>@fundakartal</p>
--- components/UserImg.js
+++ components/UserImg.js
@@ -3,7 +3,7 @@ export default function UserImg({ className }) {
     <img
       src='https://pbs.twimg.com/profile_images/1446165567187132420/8XVo47k1_400x400.jpg'
       alt='Profile image'
-      className={className}
+      className={`rounded-full ${className}`}
     />
   )
 }
