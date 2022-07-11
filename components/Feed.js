b04326e40a553943e36504bd73e4b1a5339e94b2
fundakartal <kartalfunda01@gmail.com>
2022-07-11 18:37:05 +0300

    Delete border

--- components/Feed.js
+++ components/Feed.js
@@ -5,7 +5,7 @@ import Input from 'Input'
 export default function Feed() {
   return (
     <div className='max-w-[600px] flex-grow border-l border-r border-gray-dark text-white-base xxs:sm:ml-[88px] xxs:xl:ml-[275px]'>
-      <div className='sticky top-0 z-50 flex cursor-pointer items-center border-b border-gray-dark bg-black px-4 py-3 text-white-base'>
+      <div className='sticky top-0 z-50 flex cursor-pointer items-center bg-black px-4 py-3 text-white-base'>
         <UserImg className='mr-5 h-8 w-8 xxs:sm:hidden' />
         <h2 className='overflow-hidden text-ellipsis whitespace-nowrap text-lg font-bold sm:text-xl'>
           Latest Tweets
