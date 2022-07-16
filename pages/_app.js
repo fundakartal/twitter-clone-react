76e992a57805676900ecf80590ddd660af0eebb4
fundakartal <kartalfunda01@gmail.com>
2022-07-16 23:13:07 +0300

    Add nextauth

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import '../styles/emojiPicker.css'
 import Head from 'next/head'
+import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Head>
         <title>Twitter</title>
         <link rel='icon' href='/favicon.ico' />
@@ -12,7 +13,7 @@ function MyApp({ Component, pageProps }) {
       <main className='mx-auto flex min-h-screen max-w-[1265px] pb-3'>
         <Component {...pageProps} />
       </main>
-    </>
+    </SessionProvider>
   )
 }
 
