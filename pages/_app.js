9b1eabda5bb5abdb12a4ecee749368502562e7cb
fundakartal <kartalfunda01@gmail.com>
2022-07-25 17:16:18 +0300

    Import RecoilRoot to root component

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,20 @@ import '../styles/globals.css'
 import '../styles/emojiPicker.css'
 import Head from 'next/head'
 import { SessionProvider } from 'next-auth/react'
+import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps, session }) {
   return (
     <SessionProvider session={session}>
-      <Head>
-        <title>Twitter</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <main className='mx-auto flex min-h-screen max-w-[1265px] pb-3'>
-        <Component {...pageProps} />
-      </main>
+      <RecoilRoot>
+        <Head>
+          <title>Twitter</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <main className='mx-auto flex min-h-screen max-w-[1265px] pb-3'>
+          <Component {...pageProps} />
+        </main>
+      </RecoilRoot>
     </SessionProvider>
   )
 }
