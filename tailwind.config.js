d1e9dfa2a4028ccc97fc38c6026dfdc82ee47964
fundakartal <kartalfunda01@gmail.com>
2022-07-26 13:33:43 +0300

    Delete unnecessary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,19 +12,11 @@ module.exports = {
         blue: {
           base: '#1d9bf0',
           dark: '#1b8edb',
-          light: '#8ecdf8',
-          extralight: '#d2ebfc',
         },
-        // black: {
-        //   base: 'rgb(15,20,25)',
-        //   light: '#536471',
-        // },
         gray: {
           base: 'rgba(15,20,25,0.2)',
           dark: 'rgb(47,51,54)',
-          // light: '#e7e7e8',
           light: 'rgb(113,118,123)',
-          extralight: '#f7f9f9',
         },
       },
       screens: {
