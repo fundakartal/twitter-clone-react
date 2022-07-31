65474396f1c6a7a848c1379deb4d28dece589fe2
fundakartal <kartalfunda01@gmail.com>
2022-07-31 18:20:16 +0300

    Check session

--- components/UserBox.js
+++ components/UserBox.js
@@ -1,14 +1,16 @@
 import { DotsHorizontal } from 'icons/Icon'
 import UserImg from 'UserImg'
-import { signOut, useSession } from 'next-auth/react'
+import { getProviders, signOut, useSession } from 'next-auth/react'
 
-export default function UserBox() {
-  const {data: session} = useSession()
+export default function UserBox({ providers }) {
+  const { data: session } = useSession()
+  if (!session) return <Login providers={providers} />
   return (
-    <div className='hoverAnimation hidden items-center justify-center p-3 xxs:sm:flex' onClick={signOut}>
-      <UserImg
-        className='h-10 w-10 xl:mr-3'
-      />
+    <div
+      className='hoverAnimation hidden items-center justify-center p-3 xxs:sm:flex'
+      onClick={signOut}
+    >
+      <UserImg className='h-10 w-10 xl:mr-3' />
       <div className='hidden leading-5 xl:inline '>
         <h4 className='font-bold'>{session.user.name.split(' ')[0]}</h4>
         <p className='text-gray-light'>@{session.user.tag}</p>
@@ -16,4 +18,13 @@ export default function UserBox() {
       <DotsHorizontal className='ml-auto hidden h-5 w-5 xl:inline-flex' />
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+  return {
+    props: {
+      providers,
+    },
+  }
 }
--- components/UserImg.js
+++ components/UserImg.js
@@ -1,12 +1,24 @@
-import { useSession } from 'next-auth/react'
+import { getProviders, useSession } from 'next-auth/react'
 
-export default function UserImg({ className }) {
+export default function UserImg({ className, providers }) {
   const { data: session } = useSession()
+
+  if (!session) return <Login providers={providers} />
+
   return (
     <img
-      src={session.user.image}
-      alt={session.user.name}
+      src={session?.user.image}
+      alt={session?.user.name}
       className={`rounded-full ${className}`}
     />
   )
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+  return {
+    props: {
+      providers,
+    },
+  }
 }
