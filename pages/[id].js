6c71af0124b43d21076cf9be989b366e93846ce3
fundakartal <kartalfunda01@gmail.com>
2022-08-02 00:50:36 +0300

    Edit some classes

--- pages/[id].js
+++ pages/[id].js
@@ -59,7 +59,7 @@ export default function PostPage({ providers }) {
       </Head>
       <Sidebar />
       <div className='max-w-[600px] flex-grow border-l border-r border-gray-dark text-white-base xxs:sm:ml-[88px] xxs:xl:ml-[275px]'>
-        <div className='sticky top-0 z-50 flex items-center gap-x-4 bg-black bg-opacity-90 px-1.5 py-2 text-xl font-semibold text-white-base backdrop-blur-sm'>
+        <div className='bg-[rgba(0, 0, 0, 0.65)] sticky top-0  z-50 flex items-center gap-x-4 px-1.5 py-2 text-xl font-semibold text-white-base backdrop-blur-md'>
           <div
             className='hoverAnimation flex h-9 w-9 items-center justify-center xl:px-0'
             onClick={() => router.push('/')}
